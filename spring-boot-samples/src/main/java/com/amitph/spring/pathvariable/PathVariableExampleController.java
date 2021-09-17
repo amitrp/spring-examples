4b19a74749c8827e07180b0781a531818f64a8f3
Amit Phaltankar <amitrp@users.noreply.github.com>
2021-09-18 09:33:54 +1000

    aph-13078 Path variables

---
+++ spring-boot-samples/src/main/java/com/amitph/spring/pathvariable/PathVariableExampleController.java
@@ -0,0 +1,35 @@
+package com.amitph.spring.pathvariable;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class PathVariableExampleController {
+
+    @GetMapping("/v1/students/{id}")
+    public String mapSimplePathVariable(@PathVariable String id) {
+        return "id: " + id;
+    }
+
+    @GetMapping("/v2/students/{id}")
+    public String mapPathVariableByName(
+            @PathVariable("id") String studentId) {
+        return "id: " + studentId;
+    }
+
+    @GetMapping("/v3/students/{id}/terms/{termId}")
+    public String mapPathVariableByName(
+            @PathVariable String studentId,
+            @PathVariable String termId) {
+        return "id: " + studentId + ", term id: " + termId;
+    }
+
+    @GetMapping("/v4/students/{id}/terms/{termId}")
+    public String mapPathVariableByName(
+            @PathVariable Map<String, String> pathVariables) {
+        return pathVariables.toString();
+    }
+}
